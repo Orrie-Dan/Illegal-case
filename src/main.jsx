ed671124e90ae01e8418701f68fe9bc4e74e5d2e
Orrie-Dan <d.nkusi@alustudent.com>
2026-03-06 11:00:12 +0100

    React

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import '../Case_Management_Dashboard_v2.css';
+
+ReactDOM.createRoot(document.getElementById('app-root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
